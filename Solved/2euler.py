55e26eab3e096ca3e691b70ff2b8c3655bea0086
Pawel Klim <pawelklim1@gmail.com>
2014-07-22 17:13:13 -0500

    Problem 2 solved.

---
+++ Solved/2euler.py
@@ -0,0 +1,27 @@
+# Even Fibonacci Numbers
+
+def genFibs(limit):
+    first = 1
+    second = 2
+    numbers = [1,2]
+    current = 0
+    while True:
+        current = first + second
+        if current >= limit:
+            break
+        print(current)
+        numbers.append(current)
+        first = second
+        second = current
+    return numbers
+
+def getEvens(numbers):
+    evens = []
+    for number in numbers:
+        if number % 2 == 0:
+            evens.append(number)
+    return evens
+
+    
+numbers = getEvens(genFibs(4000000))
+input(print("Sum: " + str(sum(numbers))))
